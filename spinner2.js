17cc92de37480b813f5f5c5b15f9f9bff058ae4f
Kamila Lemaire <kb.lemaire@gmail.com>
2023-04-11 15:11:43 -0600

    refactored the code further for spinner2.js

--- spinner2.js
+++ spinner2.js
@@ -1,13 +1,12 @@
 const sentence = '|/-\\|/-\\';
 let time = 0;
+let newSentence = sentence + '\n';
 
-for (let char of sentence) {
+for (let char of newSentence) {
   setTimeout(() => {
     process.stdout.write('\r' + char);
   }, time += 200);
 }
 
-setTimeout(() => {
-  console.log('\n');
-}, time);
+
 
